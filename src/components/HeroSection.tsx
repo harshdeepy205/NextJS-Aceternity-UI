7a922cd432d12556b6784e2cecdb9e79918ae1bb
harshdeepy205 <harshdeep205@outlook.com>
2024-04-15 23:29:39 +0530

    build error fix phase 1

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
                 Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.
             </p>
             <div className="mt-4">
-                <Link href={'/courses'}>
+                <Link href={"/courses"}>
                     <Button borderRadius="1.75rem"
                     className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800">
                         Explore courses
