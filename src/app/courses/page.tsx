d8a4377d0912214857398b7913105d63640d7737
harshdeepy205 <harshdeep205@outlook.com>
2024-04-20 16:06:42 +0530

    search added in the all courses

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -6,15 +6,44 @@ import { CardBody, CardContainer, CardItem } from "../../components/ui/3d-card";
 import Link from "next/link";
 import courseData from '@/data/music_courses.json'
 
+
+interface courseDataType {
+	id: number;
+	title: string;
+	slug: string;
+	description: string;
+	price: number;
+	instructor: string;
+	isFeatured: boolean;
+	image: string;
+  }
+
 function page() {
+	const [courses,setCourse] = React.useState(courseData.courses)
+	const filterCourses = (value:string)=>{
+		let courses = courseData.courses.filter((course:courseDataType)=>course.title.toLowerCase().includes(value.toLowerCase()))
+    	setCourse(courses)
+    	console.log(value)
+	}
+
   return (
 		<div className="min-h-screen bg-black py-12 pt-36">
-			<h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">
-				All courses ({courseData.courses.length})
-			</h1>
+			<div className="flex justify-between mx-16">
+				<h1 className="text-lg md:text-6xl text-center font-sans font-bold mb-8 text-white">
+					All courses ({courseData.courses.length})
+				</h1>
+				<div className="mr-24">
+					<input
+						type="text"
+						placeholder="Search"
+						className="px-4 py-2 rounded-xl  dark:bg-white dark:text-black text-black text-2xl font-bold"
+						onChange={(e) => filterCourses(e.target.value)}
+					/>
+				</div>
+			</div>
 			<div className="flex flex-wrap justify-center">
-				{courseData.courses.map((course,index) => (
-					<CardContainer className="inter-var m-4" key={index}>
+				{courses.map((course:courseDataType,index) => (
+					<CardContainer className="inter-var mx-4 my-1" key={index}>
 						<CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
 							<CardItem
 								translateZ="50"
